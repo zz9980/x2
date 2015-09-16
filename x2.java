22f93af14142ceb8690974d7d9ca6062163b7509
Bruce Alan Martin <bam59cst112day@suffolk.li>
2015-09-16 00:25:25 -0400

    Create x2.java

---
+++ x2.java
@@ -0,0 +1,79 @@
+//////// Exercise x2:  modularize exercise x1, and add dog to chase hero.
+//////// Whoever Whoever  (CST 112; today's date?)
+
+//////// Please change these to your name and today's date.
+String author=  "Whoever Whatever";
+String title=  " ??? ";
+String help=  " Click to relocate hero \n 'q' to quit; 'r' to reset. ";
+
+
+//// GLOBALS:  coordinates, speed, etc.
+float horizon;
+float x, y;       // Position.
+float dx, dy;     // Speed.
+float dogX, dogY;
+
+//// SETUP:  window size, initialization (start in middle of screen).
+void setup() {
+  size( 640,480);
+  horizon=  height/4;
+  x=  width/2;
+  y=  height/2;
+  dx=  3;
+  dy=  2;
+}
+
+//// NEXT FRAME:  scene, action, show.
+void draw() {
+  scene();
+  hero();
+  dog();
+  messages();
+}
+
+//// SCENE:  sky, sun, tree, house, etc.
+void scene() {  
+  /* INSERT YOUR CODE HERE! */
+  /* REPLACE THIS STUB! */  text( "scene", 10, 10 );
+}
+
+void messages() {
+  text( title, width/3, 20 );
+  text( help, width*2/3, 30 );
+  text( author, 10,height-20 );
+}
+
+//// ACTION:  move (x,y) coordinates of hero & dog; show them.
+void hero() {
+  /* INSERT YOUR CODE HERE! */
+  /* REPLACE THIS STUB! */  text( "[[[[ Zoog. ]]]]", 40, 20 );
+}
+void dog() {
+  dogX=  dogX + (dogX-x)/30;
+  dogY=  dogY + (dogY-x)/40;
+  /* INSERT YOUR CODE HERE! */
+  /* REPLACE THIS STUB! */  text( "woof, woof!!!", 50, 50 );
+}
+
+
+
+
+
+//////// HANDLERS:  mouse clicks, keys
+void mousePressed() {
+  x=  mouseX;                             // Set (x,y) to mouse
+  y=  mouseY;
+  //
+  dx=  random( -6, +6 );                  // random speed.
+  dy=  random( -4, +4 );
+}
+
+void keyPressed() {
+  if (key == 'q') {
+    exit();                           // press 'q' key to QUIT.
+  }
+  /* INSERT YOUR CODE HERE! */
+}
+   
+   
+
